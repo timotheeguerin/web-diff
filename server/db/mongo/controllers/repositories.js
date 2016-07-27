73ac53a9d5e6a23dc19fd1fc54874e77a84d5c67
Timothee Guerin <tiguerin@microsoft.com>
2016-07-27 13:22:41 -0700

    Hack

--- server/db/mongo/controllers/repositories.js
+++ server/db/mongo/controllers/repositories.js
@@ -14,7 +14,7 @@ export function allRepos(req, res) {
       console.log('Error on save!');
       return res.status(500).send('We failed to save for some reason');
     }
-    if(account) {
+    if (account) {
       return res.json(account.repositories);
     } else {
       return res.status(404).send("Account doesn't exists!");
@@ -45,13 +45,13 @@ export function addRepo(req, res) {
  * Add repo to account
  */
 export function getRepo(req, res) {
-    Account.findById(req.params.acctId).exec((err,account) => {
-      if(account) {
-       return res.json(account.repositories.id(req.params.id));
-      } else {
-        return res.status(404).send("Account doesn't exists!");
-      }
-    });
+  Account.findById(req.params.acctId).exec((err, account) => {
+    if (account) {
+      return res.json(account.repositories.id(req.params.id));
+    } else {
+      return res.status(404).send("Account doesn't exists!");
+    }
+  });
 }
 
 /**
@@ -61,7 +61,7 @@ export function removeRepo(req, res) {
   console.log(req.params.repoId);
   Account.update({_id: req.params.id}, {"$pull": {repositories: {_id: req.params.repoId}}}, (err, data) => {
     if (err) {
-      console.log('Error on save!'+err);
+      console.log('Error on save!' + err);
       return res.status(500).send('We failed to save for some reason');
     }
 
@@ -74,112 +74,111 @@ export function removeRepo(req, res) {
  */
 export function syncRepo(req, res) {
 
-    var repoId = req.params.id;
-    var accountId = req.params.acctId;
-    var repositoryFolder = dir+'/'+accountId+"/"+repoId+"/"+"base";
-    console.log("Blah!!!");
-    Account.findById(accountId).exec((err,account) => {
-        var repository = account.repositories.id(repoId);
-        var url= repository.url;
-        if(fs.existsSync(repositoryFolder)){
-            require('simple-git')(repositoryFolder)
-                .fetch()
-                .log(function(err, log) {
-                    console.log('update acct '+ accountId + 'update repo' + repoId);
-                    Account.update(
-                        {_id:accountId,"repositories._id":repoId},
-                        {"$set":{"repositories.$.revisions":log.all}}).exec((err,data)  => {
-                        if (err) {
-                            console.log('Error on save!'+err);
-                            return res.status(500).send('We failed to save for some reason:'+ err);
-                        }
-                        console.log(data);
-                        return res.status(200).send('Updated successfully');
-                    });
-                });
-        }
-        else{
-        fs.mkdirs(repositoryFolder, function (err) {
-            if (err) return console.error(err)
-            console.log("success!")
+  var repoId = req.params.id;
+  var accountId = req.params.acctId;
+  var repositoryFolder = dir + '/' + accountId + "/" + repoId + "/" + "base";
+  console.log("Blah!!!");
+  Account.findById(accountId).exec((err, account) => {
+    var repository = account.repositories.id(repoId);
+    var url = repository.url;
+    if (fs.existsSync(repositoryFolder)) {
+      require('simple-git')(repositoryFolder)
+        .fetch()
+        .log(function (err, log) {
+          console.log('update acct ' + accountId + 'update repo' + repoId);
+          Account.update(
+            {_id: accountId, "repositories._id": repoId},
+            {"$set": {"repositories.$.revisions": log.all}}).exec((err, data) => {
+            if (err) {
+              console.log('Error on save!' + err);
+              return res.status(500).send('We failed to save for some reason:' + err);
+            }
+            console.log(data);
+            return res.status(200).send('Updated successfully');
+          });
         });
-        require('simple-git')().clone(url,repositoryFolder).then(()=>
-        {
-            require('simple-git')(repositoryFolder)
-                .log(function(err, log) {
-                    console.log('update acct '+ accountId + 'update repo' + repoId);
-                    Account.update(
-                        {_id:accountId,"repositories._id":repoId},
-                        {"$set":{"repositories.$.revisions":log.all}}).exec((err,data)  => {
-                        if (err) {
-                            console.log('Error on save!'+err);
-                            return res.status(500).send('We failed to save for some reason:'+ err);
-                        }
-                        console.log(data);
-                        return res.status(200).send('Updated successfully');
-                    });
-                });
-        })
-        }
-    });
+    }
+    else {
+      fs.mkdirs(repositoryFolder, function (err) {
+        if (err) return console.error(err)
+        console.log("success!")
+      });
+      require('simple-git')().clone(url, repositoryFolder).then(()=> {
+        require('simple-git')(repositoryFolder)
+          .log(function (err, log) {
+            console.log('update acct ' + accountId + 'update repo' + repoId);
+            Account.update(
+              {_id: accountId, "repositories._id": repoId},
+              {"$set": {"repositories.$.revisions": log.all}}).exec((err, data) => {
+              if (err) {
+                console.log('Error on save!' + err);
+                return res.status(500).send('We failed to save for some reason:' + err);
+              }
+              console.log(data);
+              return res.status(200).send('Updated successfully');
+            });
+          });
+      })
+    }
+  });
 }
 
 /**
  * Add comparison to repository
  */
 export function addComp(req, res) {
-    var acctId = req.params.acctId;
-    var repoId = req.params.repoId;
-    var toUpdate = req.body;
-    toUpdate.target.state="stopped";
-    toUpdate.base.state="stopped";
-        Account.update(
-            {_id:acctId,"repositories._id":repoId},
-            {"$push":{"repositories.$.comparisons":req.body}}).exec((err,data)  => {
-            if (err) {
-                console.log('Error on save!'+err);
-                return res.status(500).send('We failed to save for some reason:'+ err);
-            }
-            return res.status(200).send('Updated successfully');
-        });
+  var acctId = req.params.acctId;
+  var repoId = req.params.repoId;
+  var toUpdate = req.body;
+  toUpdate.target.state = "stopped";
+  toUpdate.base.state = "stopped";
+  Account.update(
+    {_id: acctId, "repositories._id": repoId},
+    {"$push": {"repositories.$.comparisons": req.body}}).exec((err, data) => {
+    if (err) {
+      console.log('Error on save!' + err);
+      return res.status(500).send('We failed to save for some reason:' + err);
+    }
+    return res.status(200).send('Updated successfully');
+  });
 }
 
 /**
  * Get comparison from repository
  */
 export function getComp(req, res) {
-    var acctId = req.params.acctId;
-    var repoId = req.params.repoId;
-    var id = req.params.id;
+  var acctId = req.params.acctId;
+  var repoId = req.params.repoId;
+  var id = req.params.id;
 
-    Account.findById(acctId).exec((err,account) => {
-        return res.json(account.repositories.id(repoId).comparisons.id(id));
-    });
+  Account.findById(acctId).exec((err, account) => {
+    return res.json(account.repositories.id(repoId).comparisons.id(id));
+  });
 }
 
 /**
  * Get all comparisons from repository
  */
 export function getAllComp(req, res) {
-    var acctId = req.params.acctId;
-    var repoId = req.params.repoId;
-    var id = req.params.id;
+  var acctId = req.params.acctId;
+  var repoId = req.params.repoId;
+  var id = req.params.id;
 
-    Account.findById(acctId).exec((err,account) => {
-        return res.json(account.repositories.id(repoId).comparisons);
-    });
+  Account.findById(acctId).exec((err, account) => {
+    return res.json(account.repositories.id(repoId).comparisons);
+  });
 }
 
-function tryCloneRepo(url,folder,hash){
-    if (fs.existsSync(folder)) {
-        require('simple-git')(folder)
-            .pull()
-            .checkout(hash)
-    }else{
-        require('simple-git')()
-            .clone(url,folder)
-            .checkout(hash)
-    }
+function tryCloneRepo(url, folder, hash) {
+  if (fs.existsSync(folder)) {
+    require('simple-git')(folder)
+      .pull()
+      .checkout(hash)
+  } else {
+    require('simple-git')()
+      .clone(url, folder)
+      .checkout(hash)
+  }
 }
 
 /**
@@ -187,78 +186,79 @@ function tryCloneRepo(url,folder,hash){
  */
 export function startComp(req, res) {
 
-    var sys = require('sys'), exec = require('child_process').exec;
+  var sys = require('sys'), exec = require('child_process').exec;
 
-    var acctId = req.params.acctId;
-    var repoId = req.params.repoId;
-    var compId = req.params.id;
+  var acctId = req.params.acctId;
+  var repoId = req.params.repoId;
+  var compId = req.params.id;
 
-    console.log('CompId'+compId);
+  console.log('CompId' + compId);
 
-    Account.findById(acctId).exec((err,account) => {
-        var repo = account.repositories.id(repoId)
-        var comparison = account.repositories.id(repoId).comparisons.id(compId);
-        var hash1 = comparison.target.hash;
-        var hash2 = comparison.base.hash;
+  Account.findById(acctId).exec((err, account) => {
+    var repo = account.repositories.id(repoId)
+    var comparison = account.repositories.id(repoId).comparisons.id(compId);
+    var hash1 = comparison.target.hash;
+    var hash2 = comparison.base.hash;
 
-        var comparisonFolder = dir + '/' + acctId + "/" + repoId + "/comparisons";
+    var comparisonFolder = dir + '/' + acctId + "/" + repoId + "/comparisons";
 
-        var folder1 = comparisonFolder+'/'+hash1;
-        tryCloneRepo(repo.url,folder1,hash1).then(()=>{
-            exec("npm install",{cwd: folder1},function(err, stdout, stderr){
-                console.log(err + stdout + stderr)
-            }).then(()=>{
-                exec("npm run dev 3001",{cwd: folder1},function(err, stdout, stderr){
-                    console.log(err + stdout + stderr)
-                });
-            });
+    var folder1 = comparisonFolder + '/' + hash1;
+    tryCloneRepo(repo.url, folder1, hash1).then(()=> {
+      exec("npm install", {cwd: folder1}, function (err, stdout, stderr) {
+        console.log(err + stdout + stderr)
+      }).then(()=> {
+        exec("npm run dev 3001", {cwd: folder1}, function (err, stdout, stderr) {
+          console.log(err + stdout + stderr)
         });
-        var folder2 =comparisonFolder+'/'+hash2;
-        tryCloneRepo(repo.url,folder2,hash2).then(()=>{
-            exec("npm install",{cwd: folder2},function(err, stdout, stderr){
-                console.log(err + stdout + stderr)
-            }).then(()=>{
-                exec("npm run dev 3002",{cwd: folder1},function(err, stdout, stderr){
-                    console.log(err + stdout + stderr)
-                });
-            });
-        });
-        return res.status(200).send("Start successfully!");
+      });
     });
+    var folder2 = comparisonFolder + '/' + hash2;
+    tryCloneRepo(repo.url, folder2, hash2).then(()=> {
+      exec("npm install", {cwd: folder2}, function (err, stdout, stderr) {
+        console.log(err + stdout + stderr)
+      }).then(()=> {
+        exec("npm run dev 3002", {cwd: folder1}, function (err, stdout, stderr) {
+          console.log(err + stdout + stderr)
+        });
+      });
+    });
+    return res.status(200).send("Start successfully!");
+  });
 }
 
 /**
  * Stop comparison from repository (in progress)
  */
 export function stopComp(req, res) {
-    var acctId = req.params.acctId;
-    var repoId = req.params.repoId;
-    var compId = req.params.id;
+  var acctId = req.params.acctId;
+  var repoId = req.params.repoId;
+  var compId = req.params.id;
 
-    console.log('CompId'+compId);
+  console.log('CompId' + compId);
 
-    Account.update(
-        {_id:acctId,
-            "repositories._id":repoId,
-            "repositories.comparisons._id":compId
-        },
-        {"$set":{"repositories.comparisons.$$.target.state":"stopped"}}).exec((err,data)  => {
-        if (err) {
-            console.log('Error on save!'+err);
-            return res.status(500).send('We failed to save for some reason:'+ err);
-        }
-        return res.status(200).send('Updated successfully');
-    });
+  Account.update(
+    {
+      _id: acctId,
+      "repositories._id": repoId,
+      "repositories.comparisons._id": compId
+    },
+    {"$set": {"repositories.comparisons.$$.target.state": "stopped"}}).exec((err, data) => {
+    if (err) {
+      console.log('Error on save!' + err);
+      return res.status(500).send('We failed to save for some reason:' + err);
+    }
+    return res.status(200).send('Updated successfully');
+  });
 }
 export default {
-    allRepos,
-    addRepo,
-    getRepo,
-    removeRepo,
-    syncRepo,
-    addComp,
-    getComp,
-    getAllComp,
-    startComp,
-    stopComp
+  allRepos,
+  addRepo,
+  getRepo,
+  removeRepo,
+  syncRepo,
+  addComp,
+  getComp,
+  getAllComp,
+  startComp,
+  stopComp
 };
